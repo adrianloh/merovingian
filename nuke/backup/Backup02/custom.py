8e606771644f7d5904b4ac6da0bc0ce6eac8adb7
adrianloh <adrianloh@gmail.com>
2012-07-21 17:25:35 +0800

    Seeing only the stupid blonde

--- nuke/backup/Backup02/custom.py
+++
@@ -1,7 +0,0 @@
-import string
-import random
-		
-def randstring(n):
-	chars = string.ascii_letters + string.digits
-	randstr = "".join( random.sample(chars,n) )
-	return randstr
--- nuke/backup/Backup03/_init.py
+++
@@ -1,6 +0,0 @@
-import nk
-
-nuke.addAfterRender(nk.afterRender)
-nuke.addOnScriptLoad(nk.dynamicFavorites)
-nuke.addOnScriptLoad(nk.initRelativePaths)
-nuke.addOnUserCreate(nk.customizeNodeOnUserCreate)
